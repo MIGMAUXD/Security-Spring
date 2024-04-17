56c97cef721be81a98a6625c98deee7c7482a95d
MaicolRuiz01 <maicolandreyruga@ufps.edu.co>
2024-04-17 15:18:27 -0500

    hola

--- src/main/java/com/polania/demo/entities/Servicio.java
+++ src/main/java/com/polania/demo/entities/Servicio.java
@@ -10,8 +10,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -43,8 +41,5 @@ public class Servicio {
 	private String diagnostico;
 	private boolean tiempo;
 	private String demora;
-	private String tipoServicio;
-	private Integer manoObra;
-	
-	
+
 }
--- src/main/java/com/polania/demo/repository/TipoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.polania.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.polania.demo.entities.TipoServicio;
-
-public interface TipoRepository extends JpaRepository<TipoServicio, Integer>{
-
-}
